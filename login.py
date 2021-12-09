d028dfb53e6b706538d145f90cbf6e42f48c1a2e
S4mto <sametakildiz.nl0@gmail.com>
2021-12-09 10:03:55 +0100

    username & password

--- login.py
+++ login.py
@@ -1,15 +1,39 @@
 from PyQt5 import QtWidgets,uic
-from menu import main_window 
+from menu import Main_window 
+import sys
+import os
+import json
 
 class Menuscreen_window(QtWidgets.QMainWindow):
-    def __init__(self, user):
-        self.user = user 
+    def __init__(self, username=" ",password=" ",time=0,progress=0):
+        self.login(username,password,time,progress)
         super(Menuscreen_window, self).__init__() 
         uic.loadUi('login.ui', self) 
         self.login_button.clicked.connect(self.menu_screen)
         self.show() 
+    def login(self):             ### We can entry with our username and password
+        self.username = self.username_edit.text()
+        self.password = self.password_edit_2.text()
+        if not os.path.isfile('user/'+self.user_name+'.json'):
+            self.signup()
+    def signup(self):            ### sign up methode
+        self.user_info = {
+            'username': self.username,
+            'password': self.password,
+            'level': 1,
+            'time': 0
+        }
+        if self.password == "" and self.username =="":
+            self.login_info.setText("Please enter your username & password")
+        elif self.password == "":
+            self.login_info.setText("Please enter your password")
+        elif self.username =="":
+            self.login_info.setText("Please enter your username")
+        
+        else:
+            pass
     def menu_screen(self):
-        self.cams = main_window() 
+        self.cams = Main_window() 
         self.cams.show() 
         self.close() 
 if __name__ == "__main__":
