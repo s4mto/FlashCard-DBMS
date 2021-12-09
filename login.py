8f1a60e05b636494b1da00a707d6efb39df663ef
Samet <91340117+s4mto@users.noreply.github.com>
2021-12-09 10:24:58 +0100

    Update login.py

--- login.py
+++ login.py
@@ -11,27 +11,6 @@ class Menuscreen_window(QtWidgets.QMainWindow):
         uic.loadUi('login.ui', self) 
         self.login_button.clicked.connect(self.menu_screen)
         self.show() 
-    def login(self):             ### We can entry with our username and password
-        self.username = self.username_edit.text()
-        self.password = self.password_edit_2.text()
-        if not os.path.isfile('user/'+self.user_name+'.json'):
-            self.signup()
-    def signup(self):            ### sign up methode
-        self.user_info = {
-            'username': self.username,
-            'password': self.password,
-            'level': 1,
-            'time': 0
-        }
-        if self.password == "" and self.username =="":
-            self.login_info.setText("Please enter your username & password")
-        elif self.password == "":
-            self.login_info.setText("Please enter your password")
-        elif self.username =="":
-            self.login_info.setText("Please enter your username")
-        
-        else:
-            pass
     def menu_screen(self):
         self.cams = Main_window() 
         self.cams.show() 
