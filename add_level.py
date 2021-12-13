b7c520b2f59e44a69f4823425031ab6d6dedb9fa
mhmtnzly <mhmtnzly@gmail.com>
2021-12-14 00:24:34 +0100

    v.0.2

--- add_level.py
+++
@@ -1,8 +0,0 @@
-from PyQt5 import QtWidgets,uic
-import menu
-
-class add_window(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(add_window, self).__init__() 
-        uic.loadUi('ui/add.ui', self) 
-        self.show() 
