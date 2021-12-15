d24291c665baa427952397f8584742f99ba64944
S4mto <sametakildiz.nl0@gmail.com>
2021-12-15 16:12:36 +0100

    statistics

--- statistics.py
+++ statistics.py
@@ -37,7 +37,11 @@ class Statistics_window(QtWidgets.QMainWindow):
         self.level5.setText(f'{str(ranking[4][0])}')
         self.lvl5.setText(f'{str(ranking[4][1])}')
 
-        #self.rank_level.setText(f'You are {str(user_position)} th')  # user position
+        #SELECT ROW_NUMBER() OVER(),A.username, A.user_level, A.user_time FROM (select * from users order by user_level desc, user_time asc) A where username= 'ayşe'
+        cur.execute(f"""SELECT ROW_NUMBER() OVER(),A.username, A.user_level, A.user_time FROM (select * from users order by user_level desc, user_time asc) A where username= '{self.username}'
+                    """)
+        user_position= cur.fetchall()[0][0]
+        self.lvl_position.setText(f'You are {str(user_position)}. position!')  # user position
         conn.commit()
         conn.close()
 
@@ -60,9 +64,15 @@ class Statistics_window(QtWidgets.QMainWindow):
         self.succ3_1.setText(f'{str(ranking[2][1])}')
         self.succ4_1.setText(f'{str(ranking[3][1])}')
         self.succ5_1.setText(f'{str(ranking[4][1])}')
-        user_position = ''
         
-        #self.success_level.setText(f'You are {str(user_position)} th')  # user position
+        #SELECT ROW_NUMBER() over(),* FROM  (select u.username,sum(sp.percentage) from users u inner join success_percentage sp on u.user_id = sp.user_id group by u.user_id order by sum(sp.percentage) desc,u.user_time asc) A 
+#where username='mercan'
+
+        cur.execute(f"""SELECT ROW_NUMBER() over(),* FROM  (select u.username,sum(sp.percentage) from users u inner join success_percentage sp on u.user_id = sp.user_id group by u.user_id order by sum(sp.percentage) desc,u.user_time asc) A 
+where username='{self.username}'
+                    """)
+        user_position= cur.fetchall()[0][0]
+        self.prc_position.setText(f'You are {str(user_position)}. position!')  # user position
         conn.commit()
         conn.close()
         
@@ -77,13 +87,15 @@ class Statistics_window(QtWidgets.QMainWindow):
             pass
         if cur.rowcount > 0:
             self.lvl_1.setText(f'{str(ranking[0][1])}')
-            self.high1.setText(f'{str(ranking[0][2])}')
+            self.high1.setText(f'{str(ranking[0][2])} %')
         if cur.rowcount > 1:
             self.lvl_2.setText(f'{str(ranking[1][1])}')
-            self.high2.setText(f'{str(ranking[1][2])}')
+            self.high2.setText(f'{str(ranking[1][2])} %')
         if  cur.rowcount >2:
             self.lvl_3.setText(f'{str(ranking[2][1])}')
-            self.high3.setText(f'{str(ranking[2][2])}')
+            self.high3.setText(f'{str(ranking[2][2])} %')
+        conn.commit()
+        conn.close()
 
     def pers_success_lowest(self):       # Personal Success Percentage Lowest 3
         conn = psycopg2.connect(database = "flashcard",user = "postgres",host = "localhost",password = "1903")
@@ -95,10 +107,10 @@ class Statistics_window(QtWidgets.QMainWindow):
             pass
         if cur.rowcount > 0:
             self.lvl_4.setText(f'{str(ranking[0][1])}')
-            self.low1.setText(f'{str(ranking[0][2])}')
+            self.low1.setText(f'{str(ranking[0][2])} %')
         if cur.rowcount > 1:
             self.lvl_5.setText(f'{str(ranking[1][1])}')
-            self.low2.setText(f'{str(ranking[1][2])}')
+            self.low2.setText(f'{str(ranking[1][2])} %')
         if  cur.rowcount > 2:
             self.lvl_6.setText(f'{str(ranking[2][1])}')
-            self.low3.setText(f'{str(ranking[2][2])}')
+            self.low3.setText(f'{str(ranking[2][2])} %')
